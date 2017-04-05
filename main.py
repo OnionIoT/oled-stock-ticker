38f4f8d91a917f8407fa638d8041adbf8ea117dd
gbo@freecision.com <gbo@freecision.com>
2017-04-05 18:33:32 -0400

    working mvp

--- main.py
+++ main.py
@@ -1,19 +1,33 @@
 from config import config
 import parse
 import urllib
+import datetime
+import oledDriver
 
+# setup constants and data structures
 MAX_STOCKS = 7
-stockInfo = []
 
-for x in range (0, MAX_STOCKS):
-    requestTarget = config["url"] + config["stocks"][x]
-    site = urllib.urlopen(requestTarget)
-    rawResponse = site.read()
+# create a timestamp
+now = datetime.datetime.now()
+timeHeader = now.strftime("%Y-%m-%d : %X")
 
-    financeObject = parse.readGoogleFinance(rawResponse)
-    stockInfo.append({
+# prepare payload to write to screen
+payload = [timeHeader]
+
+# collect stock data
+for x in range (0, min(len(config["stocks"]), MAX_STOCKS)):
+    requestTarget = config["url"] + config["stocks"][x]     # build url with query string
+    site = urllib.urlopen(requestTarget)                    # send request
+    rawResponse = site.read()                               # read response
+    financeObject = parse.readGoogleFinance(rawResponse)    # parse the google finance object
+    
+    stockInfo = {                                      # append some of the fields to a list
         "ticker": financeObject["t"],
         "price": financeObject["l_fix"]
-    })
+    }
+    stockString = stockInfo["ticker"] + ": " + stockInfo["price"]
+    
+    payload.append(stockString)
 
-print stockInfo
+oledDriver.writeLines(payload, 0)
+
---
+++ oledDriver.py
@@ -0,0 +1,17 @@
+from OmegaExpansion import oledExp
+
+oledExp.driverInit()
+
+
+def writeLines(lines, startingRow):
+    # initialize oled to text display
+    oledExp.setTextColumns()
+    
+    # set the cursor to the beginning of the row where you want to start printing
+    oledExp.setCursor(startingRow, 0)
+    
+    # write the lines row by row
+    for i in range (0,len(lines)):
+        oledExp.setCursor(startingRow + i, 0)
+        oledExp.write(lines[i])
+        
