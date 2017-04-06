cbc4a6f5626a7f7632d92d2cf410321840d27ade
gbo@freecision.com <gbo@freecision.com>
2017-04-06 12:19:06 -0400

    added percentage change

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
+import stocks
 import datetime
 import oledDriver
-config = parse.readJsonFile('./config.json')
+config = stocks.readJsonFile('./config.json')
 
 # setup constants and data structures
 MAX_STOCKS = 7
-
-# character lengths for formatting
-L_LINE = 21     # when using oledExp.setTextColumns
-L_NAME = 4      # max 4 characters
-L_PRICE = 6     # allow up to 999.99
-L_CHANGEP = 4   # max +/- 99.99 %
-
 
 # create a timestamp
 now = datetime.datetime.now()
@@ -20,15 +14,10 @@ timeHeader = now.strftime("%Y-%m-%d : %X")
 payload = [timeHeader]
 
 # collect stock data
-stocks = stocks.getStocks(config["url"], config["stocks"])
+stockList = stocks.getStocks(config["url"], config["stocks"])
 
-for i in range(0, min(len(stocks), MAX_STOCKS)):
-    stockInfo = {                                      # append some of the fields to a list
-        "ticker": stocks[i]["t"],
-        "price": stocks[i]["l_fix"]
-    }
-    stockString = stockInfo["ticker"].ljust(L_NAME) + ": $" + stockInfo["price"].ljust(L_PRICE) + " "
-    
+for i in range(0, min(len(stockList), MAX_STOCKS)):
+    stockString = stocks.formatGoogleStockInfo(stockList[i])
     payload.append(stockString)
 
 oledDriver.writeLines(payload, 0)
--- stocks.py
+++ stocks.py
@@ -1,7 +1,11 @@
 import json
-import parse
 import urllib
 
+# character lengths for formatting
+L_LINE = 21     # when using oledExp.setTextColumns
+L_NAME = 4      # max 4 characters
+L_PRICE = 7     # allow up to 9999.99
+L_CHANGEP = 6   # max 99.99% (not including +/- sign, but including % sign)
 
 def readJsonFile(filename):
     with open(filename) as data_file:
@@ -15,11 +19,37 @@ def readGoogleFinance(response):
     data = data.replace("\n","")
     data = json.loads(data)
     return data
-    
+
+# get a list of stock info objects from api    
 def getStocks(url, stocks):
     requestTarget = url
     for i in range(0, len(stocks)):
-        requestTarget += stocks[x] + ","                    # add stock tickers to request
+        requestTarget += stocks[i] + ","                    # add stock tickers to request
     site = urllib.urlopen(requestTarget)                    # send request
     rawResponse = site.read()                               # read response
-    return parse.readGoogleFinance(rawResponse)             # returns list of stock objects
+    return readGoogleFinance(rawResponse)             # returns list of stock objects
+
+# formats the data returned from google api into a neat string
+# adjust to what you want to see
+def formatGoogleStockInfo(googleStock):
+    stockInfo = {                                      # append some of the fields to a dictionary for easier access
+        "ticker": googleStock["t"],
+        "price": googleStock["l_fix"],
+        "change": googleStock["c_fix"],
+        "changeDirection": "+" if float(googleStock["c_fix"]) >= 0 else "-",
+        "changePercentageAmount": str(abs(float(googleStock["cp_fix"])))
+    }
+    stockString = stockInfo["ticker"].ljust(L_NAME)
+    stockString += " $" + stockInfo["price"].rjust(L_PRICE)
+    stockString += " " + stockInfo["changeDirection"]
+    formattedChangePercentage = stockInfo["changePercentageAmount"] + "%"
+    stockString += formattedChangePercentage.rjust(L_CHANGEP)
+    
+    return stockString
+
+def __main__():
+    print "hello"
+    print getStocks("http://www.google.com/finance/info?q=", ["AAPL"])
+
+if __name__ == "__main__":
+    __main__()
